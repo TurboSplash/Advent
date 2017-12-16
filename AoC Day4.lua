257c58d550e0a364b3bc95e81d25f8e0a2323df3
Catalin Nedelus <catavaned@gmail.com>
2017-12-16 18:30:22 +0200

    Complete Day 4

--- AoC Day4.lua
+++ AoC Day4.lua
@@ -1,6 +1,6 @@
 local passphrases = "input_day_4"
 
---part 1
+--part 1 
 local function line_to_table(line)
   local table = {}
   local i = 1
@@ -11,10 +11,75 @@ local function line_to_table(line)
   return table
 end
 
-local function check_line (line)
+local function swap_values(v1, v2)
+  local temp = v1
+  v1 = v2
+  v2 = temp
+  return v1, v2
+end
+
+local function get_table_length(table)
+  length = 0
+  for _ in pairs(table) do 
+    length = length + 1
+  end
+  return length
+end
+
+
+local function sort_word(word)
+  local length = #word
+  repeat
+    local swapped = false
+    for i = 1, length - 1 do
+      if word[i] > word [i+1] then
+        word[i], word[i+1] = swap_values(word[i], word[i+1])
+        swapped = true
+      end
+    end
+    length = length - 1
+  until not swapped
+  return word
+end
+
+local function is_anagram(line)
+  for i = 1, #line do
+    local word_length_i = get_table_length(line[i])
+    for j = i + 1, #line do
+      local word_length_j = get_table_length(line[j])
+      if word_length_i == word_length_j then
+        local equal = 0
+        for k = 1, word_length_i do
+          if line[i][k] == line[j][k] then
+            equal = equal + 1
+            if equal == word_length_i then
+              return false
+            end
+          end
+        end
+      end
+    end
+  end
+  return true
+end
+
+local function check_anagrams(line)
   line = line_to_table(line)
-  for i=1, #line do
-    for j=i+1, #line do
+  for i = 1, #line do
+    local word = {}
+    for j = 1, #line[i] do
+      local char = line[i]:sub(j, j)
+      word[j] = string.byte(char)
+    end
+    line[i] = sort_word(word)
+  end
+  return is_anagram(line)
+end
+
+local function check_duplicates(line)
+  line = line_to_table(line)
+  for i = 1, #line do
+    for j = i + 1, #line do
       if line[i] == line[j] then
         return false
       end
@@ -25,9 +90,8 @@ end
 
 local function validate_passphrases(path)
   local valid_no = 0
-  local no_lines = 0
   for line in io.lines(path) do 
-    if check_line(line) then
+    if check_duplicates(line) and check_anagrams(line) then
       valid_no = valid_no + 1
     end
   end
@@ -35,3 +99,4 @@ local function validate_passphrases(path)
 end
 
 print(validate_passphrases(passphrases))
+
